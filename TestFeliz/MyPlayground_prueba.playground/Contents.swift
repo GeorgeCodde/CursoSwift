705e04ec7f899f1c0a345015d2e512edf3f7b7d8
Jorge Lopez <jorgels120878@gmail.com>
2016-08-30 22:19:50 -0500

    Se incluye Playground

---
+++ TestFeliz/MyPlayground_prueba.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Declaracion de variables 
+
+var cadena: String = "Hola Mundo "
+
+//Declaracion de constantes
+
+let numero: Double = 4545.333
+
+//Declaracion de arreglos
+
+var deportes = ["Basquetbol","Beisbol","Futbol","Natacion","canicas"]
+deportes.append("Ciclismo")
+print("Esta es una prueba \n " + deportes[1])
+
+//Declaracion de opcionales
+
+var opcional : String? = "Esta es una prueba"
+
+opcional = nil
+
+//-----------------------------------------------
+
+
+
+
+
+
